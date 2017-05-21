bd781544d110187ca76fdfa27357e5dadb39d37a
DESKTOP-313R62C\Louis <louis.groisne@gmail.com>
2017-05-21 15:11:32 +0200

    Increase the UI resolution

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,7 +7,7 @@ public class Menu : MonoBehaviour {
 
 
 	public void StartGame() {
-		SceneManager.LoadScene("LevelProto");
+		SceneManager.LoadScene("Level1");
 	}
  	public void Quit() {
 		Application.Quit();

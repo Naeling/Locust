7d1f4c20656829f31f61bb3b80d00ac0580b1ba9
Louis GROISNE <louis.groisne@gmail.com>
2017-06-04 14:16:47 +0200

    Add the StartArea in the level 1 and patch some Timer behaviours

---
+++ Assets/Scripts/StartArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartArea : MonoBehaviour {
+
+	public Timer timer;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// Stop the timer
+	// void OnTriggerEnter(Collider other){
+	// 	timer.StopTimer();
+	// }
+	// Launch the timer
+	void OnTriggerExit(Collider other){
+		timer.Go();
+	}
+}

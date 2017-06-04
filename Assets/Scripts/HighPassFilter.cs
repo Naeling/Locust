34b19475a1a790f811f85b91954d9ac592a513d5
Louis GROISNE <louis.groisne@gmail.com>
2017-06-04 19:17:18 +0200

    HighPass filter

---
+++ Assets/Scripts/HighPassFilter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighPassFilter : MonoBehaviour {
+
+	public AudioHighPassFilter audioFilter;
+	public Rigidbody playerRigidbody;
+
+	void Update(){
+		Vector3 forwardSpeed = Vector3.Project(playerRigidbody.velocity, playerRigidbody.transform.forward);
+		float speed = forwardSpeed.magnitude;
+		audioFilter.cutoffFrequency = 1000f - 45 * speed;
+
+	}
+
+
+}

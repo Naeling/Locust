55e3f3c7207378067c03a43c3c2bb119cc33d776
Louis GROISNE <louis.groisne@gmail.com>
2017-06-04 14:25:31 +0200

    Convert the speedometer to Int, project the speed mesured on the player's forward direction

--- Assets/Scripts/SpeedUI.cs
+++ Assets/Scripts/SpeedUI.cs
@@ -6,17 +6,20 @@ using UnityEngine.UI;
 
 public class SpeedUI : UnityEngine.MonoBehaviour {
 
-	public Rigidbody playerRigidbody;
+	public GameObject player;
+	private Rigidbody playerRigidbody;
 	private Text speedText;
 
 	// Use this for initialization
 	void Start () {
 		speedText = GetComponent<Text>();
+		playerRigidbody = player.GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		speedText.text = "Vitesse : " + System.Math.Round((decimal)playerRigidbody.velocity.magnitude, 2).ToString();
-
+		//speedText.text = "Vitesse : " + System.Math.Round((decimal)playerRigidbody.velocity.magnitude, 2).ToString();
+		Vector3 forwardSpeed = Vector3.Project(playerRigidbody.velocity, player.transform.forward);
+		speedText.text = "Vitesse : " + ((int)forwardSpeed.magnitude).ToString();
 	}
 }

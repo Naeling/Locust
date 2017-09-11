feda7383c788d5072551dec99970bce57496c376
Louis Groisne <louis.groisne@gmail.com>
2017-09-11 18:09:39 +0200

    [Credit Scene] the cancel input now redirect to the main menu

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Credits : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CrossPlatformInputManager.GetButtonDown("Cancel"))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
